13eb679e0788593fb8148d365d28921b21ef28ac
NelsonWang <wangnelson2@gmail.com>
2022-01-02 21:18:10 -0500

    inventory | welcome

--- resources/js/components/app.js
+++ resources/js/components/app.js
@@ -10,14 +10,13 @@ Vue.use(VueRouter)
 import {routes} from './routes';
 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-window.Reload = new Vue();
 
 
 const router = new VueRouter({
   routes,
   mode: 'history' 
 })
+window.Reload = new Vue();
 
 const app = new Vue({
     el: '#app',
